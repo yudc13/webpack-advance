4040c4c4d0ead5780960569664fe153ec3cb51e8
yudc13 <yudachao13@gmail.com>
2022-02-26 17:43:12 +0800

    基本使用：entry

--- 01.base/webpack.config.js
+++ 01.base/webpack.config.js
@@ -1,8 +1,21 @@
 const path = require("path");
+
 module.exports = {
 	mode: "development",
-	entry: path.join(__dirname, './src/index.js'),
+	// 字符串
+	// entry: path.resolve(__dirname, './src/index.js'),
+	// 数组
+	// entry: [path.resolve(__dirname, './src/index.js')],
+	// 对象
+	entry: {
+		// chunk的名称就是bundle
+		bundle: {
+			import: path.resolve(__dirname, './src/index.js')
+		}
+	},
 	output: {
-		path: path.join(__dirname, './dist')
+		// path 属性必须是一个绝对路径
+		path: path.resolve(__dirname, './dist'),
+		filename: "base.js",
 	}
 }
